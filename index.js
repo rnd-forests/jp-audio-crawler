2e7031444a25dc75c358289ac0c8f94bc3324ed8
Vinh Nguyen <ngocvinh.nnv@gmail.com>
2018-01-10 23:35:05 +0700

    Fix crawler

--- index.js
+++ index.js
@@ -1,27 +1,67 @@
-const { Chromeless } = require('chromeless')
+'use strict';
 
-async function run() {
-    const chromeless = new Chromeless({
-        debug: true,
-        launchChrome: true
-    })
+const fs = require('fs');
+const md5 = require('md5');
+const download = require('download');
+const puppeteer = require('puppeteer');
 
-    const audio = await chromeless
-        .goto('http://www.gavo.t.u-tokyo.ac.jp/ojad/phrasing/index')
-        .type('アクセント変形を考慮した上で文としてのピッチパターンを表示します', 'textarea[name="data[Phrasing][text]"]')
-        .click('input[value="実行"]')
-        .click('input[value="作成"')
-        .click('input[value="再生"]')
-        .wait(100)
-        .evaluate(() => {
-            return window.location
-            let baseuri = window.baseuri;
-            return baseuri + 'tmp/' + wav_filename;
-        });
+async function getSpeech(text) {
+    let audio = "";
+    let promises = [];
 
-    console.log(audio)
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
 
-    await chromeless.end()
+    // Track the requests and find the request URL to the audio file
+    page.on('request', (request) => {
+        if (request.url.endsWith('.wav')) {
+            audio = request.url;
+        }
+    });
+
+    // Go to the destination page
+    await page.goto('http://www.gavo.t.u-tokyo.ac.jp/ojad/phrasing/index');
+
+    // Fill the desired Japanese text into the textarea
+    await page.type('textarea[name="data[Phrasing][text]"]', text);
+
+    // Click on execution button
+    const executionBtnSelector = 'input[value="実行"]';
+    await page.click(executionBtnSelector);
+
+    // Synthesize the audio
+    const createBtnSelector = 'input[value="作成"]';
+    await page.waitForSelector(createBtnSelector, {visible: true});
+    await page.click(createBtnSelector);
+
+    // Play the audio in order to get the URL
+    const playBtnSelector = 'input[value="再生"]';
+    await page.waitForSelector(playBtnSelector, {visible: true});
+    await page.click(playBtnSelector);
+
+    // We're done
+    await browser.close();
+
+    return audio;
 }
 
-run().catch(console.error.bind(console))
+function downloadSpeech(text, audioDir='audio') {
+    getSpeech(text).then(audio => {
+        let target = audioDir + '/' + md5(text) + '.wav';
+        download(audio).then(stream => {
+            fs.writeFileSync(target, stream);
+        });
+    });
+}
+
+const texts = [
+    '小林さんは、その話を疑っているようだ',
+    '本当だと思っている',
+    '本当ではないと思っている',
+    'よく知っている',
+    'よく知らない'
+];
+
+for (let text of texts) {
+    downloadSpeech(text);
+}
